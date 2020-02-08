e5c0e261064f2b71d0d0c40836c4408b317699c8
re <re@re.com>
2020-02-08 11:34:29 +0330

    init commit

---
+++ src/main/kotlin/com/tapsellTask/task1/Task1Application.kt
@@ -0,0 +1,11 @@
+package com.tapsellTask.task1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Task1Application
+
+fun main(args: Array<String>) {
+	runApplication<Task1Application>(*args)
+}
